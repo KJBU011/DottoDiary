b9a07a6d89f1f98f61f56e918e066c1ddbb95469
oneofapril1 <oneofapril1@gmail.com>
2026-08-27 21:46:07 +0900

    0827mr

---
+++ client/src/components/DiariesList/DiariesList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="diarieslist" :style="dlistback">
+        <h1>
+           숲속일기장
+        </h1>
+    </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style>
+.diarieslist{
+    width: 1600px;
+    height: 800px;
+    background-image: url('../../assets/일기장목록배경.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position-x: center;
+    overflow: hidden;
+    border-radius: 40.2px;
+}
+</style>
